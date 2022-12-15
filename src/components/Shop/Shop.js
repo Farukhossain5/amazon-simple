e7397fdffffec1c24b2478f1d0746f38088834f1
Farukhossain5 <farukhossain554433.bangladesh@gmail.comm>
2022-12-15 12:43:20 +0600

    new code added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../cart/Cart';
 import Product from '../Product/Product';
+import { addToDb } from '../../utilities/fakedb';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -14,9 +15,10 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart=(product)=>{
-        console.log(product);
+        // console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
