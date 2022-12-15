c06ad8c2617ea88088bb7372933f1b16bab59e91
Farukhossain5 <farukhossain554433.bangladesh@gmail.comm>
2022-12-15 10:46:49 +0600

    new project added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css'
+const Product = ({product, handleAddToCart}) => {
+    // const {product, handleAddToCart} = props;
+    const { name, img, seller, price, ratings } = product;
+
+   
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>Name: {name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Ratings: {ratings}</small></p>
+                <p><small>Rating: {ratings}</small></p>
+                <p><small>Seller: {seller}</small></p>
+            </div>
+            <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
