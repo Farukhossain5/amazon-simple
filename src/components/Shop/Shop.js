5e7c99b09454b6ca12873f36af4552b86db6f10f
Farukhossain5 <farukhossain554433.bangladesh@gmail.comm>
2022-12-15 15:46:10 +0600

    new code adeded

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../cart/Cart';
 import Product from '../Product/Product';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -9,11 +9,25 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        console.log('product fast before fetch')
         fetch('products.json')
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data);
+                console.log('product lodeded')
+
+            });
     }, [])
 
+    useEffect(() => {
+        console.log('lacal storage first line')
+        const storedCart = getStoredCart();
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            console.log(addedProduct);
+        }
+        console.log('local store finish')
+    }, [])
     const handleAddToCart=(product)=>{
         // console.log(product);
         const newCart = [...cart, product];
